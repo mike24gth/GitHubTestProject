69ab9792a27a9f3e7fed229c2ff8e2f93f525c8a
Mahesh <mahesh@Maheshs-16gb-MacBook-Pro.local>
2018-09-01 17:26:20 -0400

    Initial commit

---
+++ src/main/java/com/mahesh/test/GitHubTestProject/App.java
@@ -0,0 +1,13 @@
+package com.mahesh.test.GitHubTestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is Mike" );
+    }
+}
