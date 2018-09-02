750ceb8f902fd22c4c70e0770865edbf17746fd6
mahesh <mahesh@192.168.0.22>
2018-09-02 02:01:22 -0400

    dvsdfvcdf

---
+++ src/main/java/com/mahesh/program/Person.java
@@ -0,0 +1,12 @@
+package com.mahesh.program;
+
+public class Person
+{
+
+	public static void main(String[] args)
+	{
+        System.out.println( "Hello World! This is Person" );
+
+	}
+
+}
--- src/main/java/com/mahesh/test/GitHubTestProject/App.java
+++ src/main/java/com/mahesh/test/GitHubTestProject/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! This is Mike" );
-        System.out.println( "Made a code change just for Git Testing!!" );
+        System.out.println( "Made a code change just for Git Testing sdgd" );
 
     }
 }
