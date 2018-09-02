92980e7f441afab4b6cc57df8d3e69e848fafa09
mahesh <mahesh@192.168.0.22>
2018-09-02 00:44:20 -0400

    creating a test branch

--- src/main/java/com/mahesh/test/GitHubTestProject/App.java
+++ src/main/java/com/mahesh/test/GitHubTestProject/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! This is Mike" );
+        System.out.println( "Made a code change just for Git Testing!!" );
+
     }
 }
